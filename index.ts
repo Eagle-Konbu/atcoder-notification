01e5b1f6f414ed3da3a1d8cc1756eaf70ed1c4eb
Atsuya Uchida <atsuya_eagle@outlook.com>
2021-09-26 01:12:27 +0900

    Modified slack message format.

--- index.ts
+++ index.ts
@@ -35,18 +35,37 @@ type ACDetail = {
 
   data.sort((a, b) => a.rank < b.rank ? -1 : 1)
 
-  let message = now.format("YYYY年MM月DD日 HH:mm現在")
+  let message = ""
   for (let i = 0; i < data.length; i++) {
     let rank = i + 1
     if (i >= 1 && data[i].rank == data[i - 1].rank) {
       rank--
     }
-    message += `\n${rank}位 (AC:${data[i].acCount}回): ${data[i].user}`
+    message += `${rank}位 (AC:${data[i].acCount}回): ${data[i].user}\n`
   }
 
   const webhook = new IncomingWebhook(process.env.SLACK_URL ?? "")
   await webhook.send({
-    text: message
+    blocks: [
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "AC数ランキング（" + now.format("YYYY年MM月DD日 HH:mm現在") + "）:deployparrot:",
+          emoji: true
+        }
+      },
+      {
+        type: "divider"
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: message
+        }
+      }
+    ]
   })
 
   console.log(message)
