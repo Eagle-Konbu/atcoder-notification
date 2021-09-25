3bc177191824fdb7c316b02336be08d234afae7f
Atsuya Uchida <atsuya_eagle@outlook.com>
2021-09-26 00:10:24 +0900

    Modified message.

--- index.ts
+++ index.ts
@@ -2,13 +2,47 @@ import { IncomingWebhook } from '@slack/webhook'
 import axios from 'axios'
 import moment from 'moment-timezone'
 import 'source-map-support'
-import data from './data.json'
 
+type ACDetail = {
+  acCount: number,
+  rank: number,
+  user: string
+}
 
 (async () => {
   const now = moment().tz("Asia/Tokyo")
-  const message = now.format("YYYY年MM月DD日 hh:mm:ss現在") + `\nAC数:${data.count}\n順位:${data.rank}`
-  console.log(message)
+  const users = ["cl17"]
+
+  let data: ACDetail[] = []
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i]
+    const detail: ACDetail = await axios.get("https://kenkoooo.com/atcoder/atcoder-api/v3/user/ac_rank", {
+      params: {
+        user: user
+      },
+      headers: {
+        "accept-encoding": "gzip"
+      }
+    }).then(res => res.data).then(json => {
+      return {
+        acCount: json.count,
+        rank: json.rank,
+        user: user
+      }
+    })
+    data.push(detail)
+  }
+
+  data.sort((a, b) => a.rank < b.rank ? -1 : 1)
+
+  let message = now.format("YYYY年MM月DD日 HH:mm現在")
+  for (let i = 0; i < data.length; i++) {
+    let rank = i + 1
+    if (i >= 1 && data[i].rank == data[i - 1].rank) {
+      rank--
+    }
+    message += `\n${rank}位 (AC:${data[i].acCount}回): ${data[i].user}`
+  }
 
   const webhook = new IncomingWebhook(process.env.SLACK_URL ?? "")
   await webhook.send({
